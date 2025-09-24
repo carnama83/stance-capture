b00a3dd4eeb441394e7bcd1f8a7a80d588468b96
carnama83 <carnama83@gmail.com>
2025-09-24 00:23:17 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import "./lib/supabaseClient"; // forces module to load, prints [ENV CHECK]
+import "./lib/supabaseClient";           // ← side-effect import so [ENV CHECK] always logs
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import "./lib/supabaseClient";
 createRoot(document.getElementById("root")!).render(<App />);
