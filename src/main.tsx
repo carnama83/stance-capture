05ffc266dec9e92203f91f384e012bd8302507cf
carnama83 <carnama83@gmail.com>
2025-09-24 00:15:45 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+import "./lib/supabaseClient"; // forces module to load, prints [ENV CHECK]
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
