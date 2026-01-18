d74ca7abd3018f3ae2dbe36a57f7f97796bf1f1d
carnama83 <carnama83@gmail.com>
2026-01-18 08:35:50 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,7 +8,7 @@
 import * as React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { getSupabase } from "./lib/supabaseClient";
+import { getSupabase } from "@/lib/supabaseClient";
 import PageLayout from "./components/PageLayout";
 import { PersonalizedFeed } from "@/components/feed/PersonalizedFeed";
 import { ThreeTierQuestionsFeed } from "@/components/question/ThreeTierQuestionsFeed";
